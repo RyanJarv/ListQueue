1dae786be1c6fe8618b6abe1fc45bf1ba329e277
Ryan Gerstenkorn <me@ryanjarv.sh>
2022-01-09 17:38:50 -0800

    Inital working commit

---
+++ main.go
@@ -0,0 +1,128 @@
+package listQueue
+
+import (
+	"sync"
+)
+
+type any interface{}
+
+type IListQueue[T any] interface {
+	Add(...T)
+}
+
+// NewListQueue returns a queue which ensures in order delivery of every past and future event to every subscriber
+//
+//    		q := listQueue.NewListQueue[int]()
+//    		q.Add(1, 2)
+//    		ch := q.Each() // Ensure Each() is called before Wait()
+//
+// q.Each() will return a channel which iterates over past and future arguments added with q.Add(...). For each itemi
+// returned from this channel, q.Done() must be called once the processing of that item is complete.
+//
+//    	    go func() {
+// 	  	    for v := range ch {
+// 	  	    	    // Prints: 		1
+// 	  	      	//				2
+// 	  	      	fmt.Println(v)
+// 	  	      	q.Done()
+// 	  	    }
+//
+//   		  q.Add(3, 4)
+//   		  for v := range q.Each() {
+//   		  	    // Prints: 		1
+//   		    	//				2
+//   		    	//				3
+//   		    	//				4
+//   		    	fmt.Println(v)
+//   		    	q.Done()
+//   		  }
+//    	}()
+//
+//    	// Waits for all events to be delivered to all channels
+//    	q.Wait()
+//
+func NewListQueue[T any]() *ListQueue[T] {
+	return &ListQueue[T]{
+		work:    &sync.WaitGroup{},
+		items:   make([]T, 0, 1000),
+		c:       sync.NewCond(&sync.Mutex{}),
+		notDone: true,
+	}
+}
+
+type IWaitGroup interface {
+	Add(int)
+	Done()
+	Wait()
+}
+
+type ListQueue[T any] struct {
+	work        IWaitGroup
+	items       []T
+	c           *sync.Cond
+	notDone     bool
+	subscribers int
+}
+
+// sendFrom takes a starting index and a channel and sends all items that have been added since the given index
+func (s *ListQueue[T]) sendFrom(start int, ch chan T) int {
+	var i int
+	var v T
+	for i, v = range s.items[start:] {
+		ch <- v
+	}
+	return i
+}
+
+// next waits for the next batch of items after index i to be placed on the list.
+func (s *ListQueue[T]) next(i int) bool {
+	s.c.L.Lock()
+	s.c.Wait()
+	s.c.L.Unlock()
+	return s.notDone && len(s.items) != i
+}
+
+// Each returns a channel which will be sent all current and future items passed to Add in the order they were added.
+func (s *ListQueue[T]) Each() chan T {
+	// For each new subscriber all items need to be returned.
+	s.subscribers++
+	s.work.Add(len(s.items))
+
+	itemCh := make(chan T, 0)
+	go func() {
+		// Catch this subscriber up to the current tip.
+		i := s.sendFrom(0, itemCh)
+
+		// Wait to be notified of more items, then iterate over them.
+		for s.next(i) {
+			i = s.sendFrom(i, itemCh)
+		}
+		close(itemCh)
+	}()
+
+	return itemCh
+}
+
+// Done must be called after processing is finished for each item sent to the channel returned by Each.
+func (s *ListQueue[T]) Done() {
+	s.work.Done()
+}
+
+// Wait must be called after Each() for channels returned by Each() to be closed on completion.
+//
+// If called before Each() this function will return immediately.
+func (s *ListQueue[T]) Wait() {
+	s.work.Wait() // Wait for all items to be returned.
+	s.notDone = false
+	s.c.Broadcast() // Release any goroutines waiting on s.next()
+}
+
+// Add adds all passsed arguments to the list and wakes up any paused goroutines to continue reading from the list.
+func (s *ListQueue[T]) Add(items ...T) {
+	// An item needs to be returned once for each subscriber.
+	s.work.Add(len(items) * s.subscribers)
+	s.c.L.Lock() // TODO: I don't think we actually need to lock here.
+	s.items = append(s.items, items...)
+	s.c.Broadcast()
+	s.c.L.Unlock()
+}
---
+++ main_test.go
@@ -0,0 +1,274 @@
+package listQueue
+
+import (
+	"github.com/stretchr/testify/mock"
+	"reflect"
+	"sync"
+	"testing"
+)
+
+type MockWaitGroup struct{ mock.Mock }
+
+func (m *MockWaitGroup) Add(i int) { m.Called(i) }
+func (m *MockWaitGroup) Done()     { m.Called() }
+func (m *MockWaitGroup) Wait()     { m.Called() }
+
+func TestListQueue_Add(t *testing.T) {
+	type fields struct {
+		work        IWaitGroup
+		items       []string
+		c           *sync.Cond
+		notDone     bool
+		subscribers int
+	}
+	type args struct {
+		items            []string
+		expectedWork     int
+		expectedNumItems int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		q    *ListQueue[string]
+	}{
+		{
+			name: "does nothing with no arguments",
+			q: &ListQueue[string]{
+				work:    &MockWaitGroup{},
+				items:   make([]string, 0, 1000),
+				c:       sync.NewCond(&sync.Mutex{}),
+				notDone: true,
+			},
+			args: args{
+				items:            []string{},
+				expectedWork:     0,
+				expectedNumItems: 0,
+			},
+		},
+		{
+			name: "updates items and adds no expectedWork when there are no subscribers",
+			q: &ListQueue[string]{
+				work:    &MockWaitGroup{},
+				items:   make([]string, 0, 1000),
+				c:       sync.NewCond(&sync.Mutex{}),
+				notDone: true,
+			},
+			args: args{
+				items:            []string{"a", "b"},
+				expectedWork:     0,
+				expectedNumItems: 2,
+			},
+		},
+		{
+			name: "when there is one subscriber and 3 args add 3 items of expectedWork",
+			q: &ListQueue[string]{
+				work:        &MockWaitGroup{},
+				items:       make([]string, 0, 1000),
+				c:           sync.NewCond(&sync.Mutex{}),
+				notDone:     true,
+				subscribers: 1,
+			},
+			args: args{
+				items:            []string{"a", "b", "c"},
+				expectedWork:     3,
+				expectedNumItems: 3,
+			},
+		},
+		{
+			name: "when called with existing items and 2 subscribers, 2 arguments add 4 items of expectedWork",
+			q: &ListQueue[string]{
+				work:        &MockWaitGroup{},
+				items:       []string{"a", "b"},
+				c:           sync.NewCond(&sync.Mutex{}),
+				notDone:     true,
+				subscribers: 2,
+			},
+			args: args{
+				items:            []string{"c", "d"},
+				expectedWork:     4,
+				expectedNumItems: 4,
+			},
+		},
+		{
+			name: "when called with 2 args and 2 existing items, items becomes 4",
+			q: &ListQueue[string]{
+				work:        &MockWaitGroup{},
+				items:       []string{"a", "b"},
+				c:           sync.NewCond(&sync.Mutex{}),
+				notDone:     true,
+				subscribers: 0,
+			},
+			args: args{
+				items:            []string{"c", "d"},
+				expectedWork:     0,
+				expectedNumItems: 4,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			origItems := make([]string, len(tt.q.items))
+			copy(origItems, tt.q.items)
+			tt.q.work.(*MockWaitGroup).On("Add", tt.args.expectedWork)
+			tt.q.Add(tt.args.items...)
+			if tt.args.expectedNumItems != len(tt.q.items) {
+				t.Errorf("len(tt.args.expectedNumItems) != len(tt.q.items): %d != %d", tt.args.expectedNumItems, len(tt.q.items))
+			}
+			for i, v := range append(origItems, tt.args.items...) {
+				//off := len(tt.q.items) - (len(tt.args.items) - i) // Only check added items
+				if v != tt.q.items[i] {
+					t.Errorf("item tt.args.items[%d] != tt.q.items[%d]: %s != %s", i, i, v, tt.q.items[i])
+				}
+			}
+			tt.q.work.(*MockWaitGroup).AssertNumberOfCalls(t, "Add", 1)
+		})
+	}
+}
+
+// TODO: Finish tests
+func TestListQueue_Each(t *testing.T) {
+	type fields struct {
+		work        *sync.WaitGroup
+		items       []string
+		c           *sync.Cond
+		notDone     bool
+		subscribers int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   chan string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &ListQueue[string]{
+				work:        tt.fields.work,
+				items:       tt.fields.items,
+				c:           tt.fields.c,
+				notDone:     tt.fields.notDone,
+				subscribers: tt.fields.subscribers,
+			}
+			if got := s.Each(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Each() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestListQueue_Wait(t *testing.T) {
+	type fields struct {
+		work        *sync.WaitGroup
+		items       []string
+		c           *sync.Cond
+		notDone     bool
+		subscribers int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &ListQueue[string]{
+				work:        tt.fields.work,
+				items:       tt.fields.items,
+				c:           tt.fields.c,
+				notDone:     tt.fields.notDone,
+				subscribers: tt.fields.subscribers,
+			}
+			s.Wait()
+		})
+	}
+}
+
+func TestListQueue_next(t *testing.T) {
+	type fields struct {
+		work        *sync.WaitGroup
+		items       []string
+		c           *sync.Cond
+		notDone     bool
+		subscribers int
+	}
+	type args struct {
+		i int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &ListQueue[string]{
+				work:        tt.fields.work,
+				items:       tt.fields.items,
+				c:           tt.fields.c,
+				notDone:     tt.fields.notDone,
+				subscribers: tt.fields.subscribers,
+			}
+			if got := s.next(tt.args.i); got != tt.want {
+				t.Errorf("next() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestListQueue_sendFrom(t *testing.T) {
+	type fields struct {
+		work        *sync.WaitGroup
+		items       []string
+		c           *sync.Cond
+		notDone     bool
+		subscribers int
+	}
+	type args struct {
+		start int
+		ch    chan string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &ListQueue[string]{
+				work:        tt.fields.work,
+				items:       tt.fields.items,
+				c:           tt.fields.c,
+				notDone:     tt.fields.notDone,
+				subscribers: tt.fields.subscribers,
+			}
+			if got := s.sendFrom(tt.args.start, tt.args.ch); got != tt.want {
+				t.Errorf("sendFrom() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewListQueue(t *testing.T) {
+	tests := []struct {
+		name string
+		want *ListQueue[string]
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewListQueue[string](); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewListQueue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
